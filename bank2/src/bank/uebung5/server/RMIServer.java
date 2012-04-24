b28bee41b56776e4856a67ab5af6249056a3c1a8
Pascal Schwarz <iso@thinktux.net>
2012-04-24 10:51:12 +0200

    Gerüst weiter, funzt noch überhaupt nicht :p

---
+++ bank2/src/bank/uebung5/server/RMIServer.java
@@ -0,0 +1,25 @@
+package bank.uebung5.server;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
+public class RMIServer {
+
+	public static void main(String[] args) throws RemoteException, MalformedURLException {
+		// try to create rmi registry withing this process
+		try {
+			System.out.println(RMIServer.class.getName()+".main: creating registry");
+			LocateRegistry.createRegistry(1099);
+		} catch (RemoteException e) {
+			System.out.println(">> registry could not be exported");
+			System.out.println(">> probably another registry already runs on 1099");
+		}
+
+		BankRMI bank = new BankRMI();
+		Naming.rebind("rmi://localhost:1099/BankService", bank);
+		System.out.println("Bank server started...");
+	}
+
+}
