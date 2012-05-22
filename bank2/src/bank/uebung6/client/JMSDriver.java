140ce31f04c47d731ab0b8b3dd41b8141d98fa51
Pascal Schwarz <iso@thinktux.net>
2012-05-22 10:09:54 +0200

    vorbereitungen übung 6 ;)

---
+++ bank2/src/bank/uebung6/client/JMSDriver.java
@@ -0,0 +1,28 @@
+package bank.uebung6.client;
+
+import java.io.IOException;
+
+import bank.Bank;
+import bank.BankDriver;
+
+public class JMSDriver implements BankDriver {
+
+	@Override
+	public void connect(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void disconnect() throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Bank getBank() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ bank2/src/bank/uebung6/srv/JMSServer.java
@@ -0,0 +1,13 @@
+package bank.uebung6.srv;
+
+public class JMSServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
