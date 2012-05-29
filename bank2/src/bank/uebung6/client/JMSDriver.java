41d26d72f801e6409c10ecf95410e6d975dc76d1
Pascal Schwarz <iso@thinktux.net>
2012-05-29 15:12:36 +0200

    eberle test ond fix! :D

--- bank2/src/bank/uebung6/client/JMSDriver.java
+++ bank2/src/bank/uebung6/client/JMSDriver.java
@@ -1,11 +1,14 @@
 package bank.uebung6.client;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import javax.jms.Connection;
 import javax.jms.DeliveryMode;
 import javax.jms.Destination;
 import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.ObjectMessage;
 import javax.jms.QueueConnection;
@@ -26,6 +29,8 @@ public class JMSDriver implements CommandDriver {
 	private CommandBank proxyBank;
 	private MessageProducer producer;
 	private Session session;
+	private MessageConsumer receiver;
+	private Destination answerDestination;
 	
 	@Override
 	public void connect(String[] args) throws IOException {
@@ -43,6 +48,9 @@ public class JMSDriver implements CommandDriver {
 			Destination destination = session.createQueue("JMSBank");
 			producer = session.createProducer(destination);
 			producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+			
+			answerDestination = session.createQueue("JMSBankClient-" + UUID.randomUUID());
+			receiver = session.createConsumer(answerDestination);
 		} catch (JMSException e) {
 			System.err.println("could not connect to BankJMS");
 			e.printStackTrace();
@@ -53,6 +61,11 @@ public class JMSDriver implements CommandDriver {
 	@Override
 	public void disconnect() throws IOException {
 		proxyBank = null;
+		try {
+			session.close();
+		} catch (JMSException e) {
+			throw new IOException(e);
+		}
 	}
 
 	@Override
@@ -64,16 +77,20 @@ public class JMSDriver implements CommandDriver {
 	public ReturnValue executeCommand(Command cmd) {
 		try {
 			ObjectMessage msg = session.createObjectMessage(cmd);
+			msg.setJMSReplyTo(answerDestination);
 			producer.send(msg);
 			
-			// TODO: receive result (sync) and return the ReturnValue
+			// get answer and process
+			Message respMsg = receiver.receive(500);
+			if (respMsg == null || !(respMsg instanceof ObjectMessage)) {
+				return new ReturnValueDefault(false, new IOException("received response not OK! " + respMsg), null);
+			}
+			
+			return (ReturnValue) ((ObjectMessage) respMsg).getObject();
 			
 		} catch (JMSException e) {
 			return new ReturnValueDefault(false, new IOException(e), null);
 		}
-		
-		
-		return null;
 	}
 
 }
